d689729c1fbeb1d0e8f43adbd0f57208d55c490e
JoaoVictorRosmaninho <joaovictorrosmaninho@gmail.com>
2022-11-06 20:02:56 -0300

    passing norminette

--- headers/minitalk.h
+++ headers/minitalk.h
@@ -1,5 +1,17 @@
-#ifndef SERVER_H
-# define SERVER_H 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jv <jv@student.42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/06 20:01:41 by jv                #+#    #+#             */
+/*   Updated: 2022/11/06 20:02:30 by jv               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H 
 
 # include <unistd.h>
 # include <sys/types.h>
@@ -8,5 +20,5 @@
 
 # define BUFFSIZE 1024
 
-void show_message(const char *msg);
+void	show_message(const char *msg);
 #endif
